494106e6e4dbd4da3b0916f0ac3fa10a13bbae2f
Your Name <you@example.com>
2023-11-14 16:46:09 +0600

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {resolve} from "node";
+import {resolve} from "path";
 
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: 'localhost',
+    port: 3000
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
     }
-  },
-  server: {
-    host: 'localhost',
-    port: 3000
   }
 })
